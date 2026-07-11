455530a0fd91e420afa02763e9d6ff064a498c23
Ryoka <tenkaro@icloud.com>
2026-07-11 15:05:11 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./hero.css";
 import "./about.css";
+import "./episodes.css";
 
 const inter = Inter({
   subsets: ["latin"],
